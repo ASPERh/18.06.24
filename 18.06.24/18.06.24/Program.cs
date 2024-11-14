05dd1a191921626afa66d7c0631c4f54ceeca905
ASPER <144143767+ASPERh@users.noreply.github.com>
2024-11-14 15:55:38 +0000

    Add files via upload

---
+++ 18.06.24/18.06.24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW.Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int[] arr = { 1, 11, 21, 1211, 111221, 312211, 13112221 };
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
